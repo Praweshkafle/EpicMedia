b1c286ee8a641d134fd87a0e6f454b19f7c68cae
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-11-30 14:31:50 +0545

    entities added

---
+++ EpicMedia/EpicMedia.Api/Entity/Posts.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpicMedia.Api.Entity
+{
+    public class Posts
+    {
+        public ObjectId Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+        public string Image { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        public int AuthorId { get; set; }
+
+    }
+}
---
+++ EpicMedia/EpicMedia.Api/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpicMedia.Api.Entity
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; } 
+
+        public string ProfilePicture { get; set; } = "";
+    }
+}
