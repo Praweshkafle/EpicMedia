3b62e44f19c7aa8c29a64e5b10826f8f339e23a3
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-11-30 14:31:57 +0545

    repo added

---
+++ EpicMedia/EpicMedia.Api/Repository/Implementation/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using EpicMedia.Api.Entity;
+using EpicMedia.Api.Repository.Interface;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace EpicMedia.Api.Repository.Implementation
+{
+    public class UserRepository:IUserRepository
+    {
+        private readonly IMongoCollection<User> _collection;
+
+        public UserRepository(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<User>("Users");
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<User> GetById(ObjectId id)
+        {
+            return await _collection.Find(u => u.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            return await _collection.Find(u => u.Email == email).FirstOrDefaultAsync();
+        }
+
+        public async Task Create(User user)
+        {
+            await _collection.InsertOneAsync(user);
+        }
+
+        public async Task<bool> Update(ObjectId id, User user)
+        {
+            var result = await _collection.ReplaceOneAsync(u => u.Id == id, user);
+            return result.ModifiedCount > 0;
+        }
+
+        public async Task<bool> Delete(ObjectId id)
+        {
+            var result = await _collection.DeleteOneAsync(u => u.Id == id);
+            return result.DeletedCount > 0;
+        }
+    }
+}
---
+++ EpicMedia/EpicMedia.Api/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using EpicMedia.Api.Entity;
+using MongoDB.Bson;
+
+namespace EpicMedia.Api.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(ObjectId id);
+        Task<User> GetByEmail(string email);
+        Task Create(User user);
+        Task<bool> Update(ObjectId id, User user);
+        Task<bool> Delete(ObjectId id);
+    }
+}
