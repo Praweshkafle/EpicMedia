6681a16de1120ec8446d1c14e4ec01e07057975a
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-11-30 14:31:34 +0545

    controllers added

---
+++ EpicMedia/EpicMedia.Api/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using EpicMedia.Api.Entity;
+using EpicMedia.Api.Repository.Interface;
+using EpicMedia.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpicMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> Register([FromBody] UserDto userDto)
+        {
+            try
+            {
+                if (userDto != null)
+                {
+                    var user = new User
+                    {
+                        Email = userDto.Email,
+                        Password = userDto.Password,
+                        ProfilePicture = userDto.ProfilePicture,
+                        Username = userDto.Username,
+                    };
+                    await _userRepository.Create(user);
+                    return Ok();
+                }
+                throw new Exception("Something went wrong");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
