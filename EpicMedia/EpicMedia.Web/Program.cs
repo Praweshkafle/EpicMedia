74efd9210222df7c1806e5da5a96c7a34f89f541
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-12-05 22:54:17 +0545

    configured client site

--- EpicMedia/EpicMedia.Web/Program.cs
+++ EpicMedia/EpicMedia.Web/Program.cs
@@ -1,6 +1,9 @@
+using Blazored.LocalStorage;
 using EpicMedia.Web;
 using EpicMedia.Web.Services.Implementation;
 using EpicMedia.Web.Services.Interface;
+using EpicMedia.Web.Shared.Manager;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,4 +13,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7270/") });
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+builder.Services.AddAuthorizationCore();
+
+
 await builder.Build().RunAsync();
