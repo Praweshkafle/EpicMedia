8939c9f80945c00c7758c549af08196c7a0b3363
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-12-07 23:28:10 +0545

    register valid model aded

---
+++ EpicMedia/EpicMedia.Web/ViewModels/RegisterVm.cs
@@ -0,0 +1,10 @@
+﻿namespace EpicMedia.Web.ViewModels
+{
+    public class RegisterVm
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfilePicture { get; set; } = "";
+    }
+}
---
+++ EpicMedia/EpicMedia.Web/ViewModels/Validation/RegisterValidation.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace EpicMedia.Web.ViewModels.Validation
+{
+    public class RegisterValidation:AbstractValidator<RegisterVm>
+    {
+        public RegisterValidation()
+        {
+            RuleFor(a => a.Username).NotEmpty();
+            RuleFor(a => a.Email).NotEmpty().EmailAddress();
+            RuleFor(a => a.Password).NotEmpty().WithMessage("Your password cannot be empty")
+                    .MinimumLength(6).WithMessage("Your password length must be at least 6.")
+                    .MaximumLength(16).WithMessage("Your password length must not exceed 16.")
+                    .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+                    .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+                    .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
+                    .Matches(@"[\@\!\?\*\.]+").WithMessage("Your password must contain at least one (@!? *.).");
+
+        }
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<RegisterVm>.CreateWithOptions((RegisterVm)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+}
