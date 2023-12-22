537f8ff820949836ccb1ed3b782967b4c7d15606
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2023-12-22 11:07:35 +0545

    register vm added

--- EpicMedia/EpicMedia.Web/ViewModels/RegisterVm.cs
+++ EpicMedia/EpicMedia.Web/ViewModels/RegisterVm.cs
@@ -1,10 +1,20 @@
-﻿namespace EpicMedia.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EpicMedia.Web.ViewModels
 {
     public class RegisterVm
     {
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
         public string Password { get; set; }
+
         public string ProfilePicture { get; set; } = "";
     }
 }
