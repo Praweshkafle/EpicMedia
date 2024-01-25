68726c79e4c3aa42b69d27828fb6052fe03dc556
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2024-01-25 15:58:57 +0545

    post controller updated added getall post

--- EpicMedia/EpicMedia.Api/Controllers/PostController.cs
+++ EpicMedia/EpicMedia.Api/Controllers/PostController.cs
@@ -1,9 +1,12 @@
 ﻿using EpicMedia.Api.Entity;
+using EpicMedia.Api.Filemanager;
 using EpicMedia.Api.Repository.Interface;
 using EpicMedia.Models.Dto;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using MongoDB.Bson;
 
 namespace EpicMedia.Api.Controllers
@@ -11,39 +14,47 @@ namespace EpicMedia.Api.Controllers
     [Route("api/posts")]
     public class PostController : ControllerBase
     {
+        private readonly IFileManager _fileManager;
         private readonly IPostRepository _postRepository;
 
-        public PostController(IPostRepository postRepository)
+        public PostController(IPostRepository postRepository ,IFileManager fileManager)
         {
             _postRepository = postRepository;
+            _fileManager = fileManager;
         }
 
         [HttpPost]
         [Route("create")]
         [Authorize]
-        public async Task<IActionResult> create([FromBody]PostDto postDto)
+        public async Task<IActionResult> create([FromForm] string postdto, [FromForm] IFormFile file)
         {
             try
             {
+                PostDto postDto =  Newtonsoft.Json.JsonConvert.DeserializeObject<PostDto>(postdto);
                 if(postDto == null) { return BadRequest(new { Sucess = false, Message = "Error occured!" }); }
-
+                string imagePath="";
+                if (file != null)
+                {
+                    var prefix = Guid.NewGuid();
+                    imagePath = _fileManager.saveImageAndGetFileName(file, prefix.ToString());
+                }
                 var post = new Posts
                 {
                     Content = postDto.Content,
                     CreatedAt = DateTime.Now,
-                    Image = postDto.Image,
+                    Image = imagePath,
                     Likes = postDto.Likes,
                     User = postDto.User,
                     Comments = null
                 };
+               
                 await _postRepository.Create(post);
 
                 return Ok(new {Sucess=true,Message="Post created successfully!"});
             }
             catch (Exception)
             {
-
-                throw;
+                return BadRequest(new { Sucess = false, Message = "Error occured!!" });
             }
         }
 
@@ -74,6 +85,39 @@ namespace EpicMedia.Api.Controllers
 
                 throw;
             }
+        }
+
+        [HttpGet]
+        [Route("getallpost")]
+        public async Task<IActionResult> getallpost()
+        {
+            try
+            {
+                var posts = await _postRepository.GetAll();
+                if (posts == null) return NotFound();
+                string imgurl = GetPostImageFullPath();
+                var postDtos = posts.Select(post => new PostDto
+                {
+                    Id = post.Id.ToString(), 
+                    Content = post.Content,
+                    User = post.User,
+                    Image = imgurl+post.Image,
+                    Likes = post.Likes,
+                    CreatedAt = post.CreatedAt
+                }).ToList();
+
+                return Ok(postDtos);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private string GetPostImageFullPath()
+        {
+            return "https://localhost:7270/Images/Custom/";
         }
     }
 }
