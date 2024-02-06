6fc2609ac6fb7ce9cfb63137ddf3b0d9b5490ac6
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2024-02-06 14:23:14 +0545

    reply entity added

--- EpicMedia/EpicMedia.Api/Entity/Reply.cs
+++ EpicMedia/EpicMedia.Api/Entity/Reply.cs
@@ -1,14 +1,12 @@
-﻿using EpicMedia.Api.Entity;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 
 namespace EpicMedia.Api.Entity
 {
-    public class Comment
+    public class Reply
     {
         public ObjectId Id { get; set; }
         public string? Text { get; set; }
         public string? User { get; set; }
-        public List<Reply> Replies { get; set; } = new List<Reply>();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
