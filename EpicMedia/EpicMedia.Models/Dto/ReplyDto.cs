d30d2277d2d58f77ecd0035ca588207880a3a553
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2024-02-06 14:23:22 +0545

    dto updated

--- EpicMedia/EpicMedia.Models/Dto/ReplyDto.cs
+++ EpicMedia/EpicMedia.Models/Dto/ReplyDto.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace EpicMedia.Models.Dto
 {
-    public class CommentDto
+    public class ReplyDto
     {
         public string Id { get; set; }
-        public string Text { get; set; }
-        public string User { get; set; }
-        public string postId { get; set; }
+        public string? Text { get; set; }
+        public string? User { get; set; }
+        public List<ReplyDto> Replies { get; set; } = new List<ReplyDto>();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
