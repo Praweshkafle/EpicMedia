8fc5a8686e267cde85c681f9cf8efb3d58d925f7
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2024-02-08 11:26:59 +0545

    GG

--- EpicMedia/EpicMedia.Models/Dto/CommentDto.cs
+++ EpicMedia/EpicMedia.Models/Dto/CommentDto.cs
@@ -13,6 +13,7 @@ namespace EpicMedia.Models.Dto
         public string Text { get; set; }
         public string User { get; set; }
         public string postId { get; set; }
+        public List<ReplyDto> Replies { get; set; } = new List<ReplyDto>();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
--- EpicMedia/EpicMedia.Models/Dto/ReplyDto.cs
+++ EpicMedia/EpicMedia.Models/Dto/ReplyDto.cs
@@ -12,7 +12,7 @@ namespace EpicMedia.Models.Dto
         public string Id { get; set; }
         public string? Text { get; set; }
         public string? User { get; set; }
-        public List<ReplyDto> Replies { get; set; } = new List<ReplyDto>();
+        public string ParentCommentId { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
