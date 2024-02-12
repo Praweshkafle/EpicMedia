e05cdc8e44e653975e5bd6182c76ac423cef3fc2
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2024-02-12 21:38:55 +0545

    model updated

--- EpicMedia/EpicMedia.Api/Entity/Reply.cs
+++ EpicMedia/EpicMedia.Api/Entity/Reply.cs
@@ -7,6 +7,8 @@ namespace EpicMedia.Api.Entity
         public ObjectId Id { get; set; }
         public string? Text { get; set; }
         public string? User { get; set; }
+        public string postId { get; set; }
+        public string ParentCommentId { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
--- EpicMedia/EpicMedia.Models/Dto/ReplyDto.cs
+++ EpicMedia/EpicMedia.Models/Dto/ReplyDto.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace EpicMedia.Models.Dto
 {
     public class ReplyDto
     {
+        [Key]
         public string Id { get; set; }
         public string? Text { get; set; }
         public string? User { get; set; }
+        public string postId { get; set; }
         public string ParentCommentId { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
